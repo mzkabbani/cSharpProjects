d2d97cb63ba1b71dc58247b8732eb31d25def7ee
mzkabbani <mzkabbani@gmail.com>
2012-07-27 09:28:04 +0300

    added

---
+++ RFT-Replaces/XmlParsersAndUi/Forms/Multi-TestCaseForm.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace XmlParsersAndUi.Forms {
+    partial class Multi_TestCaseForm {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Multi_TestCaseForm));
+            this.txtInputDir = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.txtOutputDir = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtInputDir
+            // 
+            this.txtInputDir.Location = new System.Drawing.Point(70, 12);
+            this.txtInputDir.Name = "txtInputDir";
+            this.txtInputDir.Size = new System.Drawing.Size(380, 20);
+            this.txtInputDir.TabIndex = 0;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(197, 64);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // txtOutputDir
+            // 
+            this.txtOutputDir.Location = new System.Drawing.Point(70, 38);
+            this.txtOutputDir.Name = "txtOutputDir";
+            this.txtOutputDir.Size = new System.Drawing.Size(380, 20);
+            this.txtOutputDir.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Input Dir:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Output Dir:";
+            // 
+            // Multi_TestCaseForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(468, 98);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtOutputDir);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.txtInputDir);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Multi_TestCaseForm";
+            this.Text = "Multi_TestCaseForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInputDir;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox txtOutputDir;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ RFT-Replaces/XmlParsersAndUi/Forms/Multi-TestCaseForm.cs
@@ -0,0 +1,294 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace XmlParsersAndUi.Forms {
+    public partial class Multi_TestCaseForm : Form {
+        public Multi_TestCaseForm() {
+            InitializeComponent();
+        }
+
+
+
+        private void StartSplittingFile(string fileName, string sessionKey, out List<checkboxItems> items) {
+            if (!Directory.Exists(txtOutputDir.Text)) {
+                Directory.CreateDirectory(txtOutputDir.Text);
+            }
+            string fileRead = string.Empty;
+
+            items = new List<checkboxItems>();
+            StreamReader reader = new StreamReader(fileName);
+            string eventsFile = "<Steps label=\"Automatically Generated\">";
+            try {
+                fileRead = reader.ReadToEnd();
+                MatchCollection collection = Regex.Matches(fileRead, "<Comment.*EventID=\"(\\d+)\".*>(.*)</Comment>");
+                List<string> matches = new List<string>();
+                Dictionary<string, string> matchesAndId = new Dictionary<string, string>();
+                for (int i = 0; i < collection.Count; i++) {
+                    matches.Add(collection[i].Groups[2].ToString());
+                    matchesAndId.Add(collection[i].Groups[1].ToString(), collection[i].Groups[2].ToString());
+                    //     txtOutput.Text = txtOutput.Text + "\r\n" + collection[i].Groups[1].ToString();
+                }
+
+
+                string newFileRead = Regex.Replace(fileRead, "<Comment.*</Comment>", "</Events></MXClientScript><!-- --><MXClientScript><Events>");
+                string[] test = new string[] { "<!-- -->" };
+                string[] splitArray = newFileRead.Split(test, StringSplitOptions.RemoveEmptyEntries);
+
+               
+                for (int j = 0; j < splitArray.Length; j++) {
+                    if (j < collection.Count) {
+                        StreamWriter writer = new StreamWriter(txtOutputDir.Text + @"\step" + (j + 1) + "_events.xml");
+                        StreamWriter guiWriter = new StreamWriter(txtOutputDir.Text + @"\step" + (j + 1) + "_gui.xml");
+                        StreamWriter customsWriter = new StreamWriter(txtOutputDir.Text + @"\step" + (j + 1) + "_customs.xml");
+                        try {
+                            guiWriter.Write(" ");
+                            customsWriter.Write(@"<Customs></Customs>");
+                            writer.Write(splitArray[j]);
+                            if (string.IsNullOrEmpty(sessionKey)) {
+
+                                eventsFile = eventsFile + "\r\n <Step id=\"" + (j + 1) + "\" label=\"" + collection[j].Groups[1].ToString() + "\" events=\"step" + (j + 1) + "_events.xml\" gui=\"step" + (j + 1) + "_gui.xml\" customs=\"step" + (j + 1) + "_customs.xml\"></Step>";
+
+                            } else {
+                                eventsFile = eventsFile + "\r\n <Step id=\"" + (j + 1) + "\" sessionKey=\"" + sessionKey + "\" label=\"" + collection[j].Groups[1].ToString() + "\" events=\"step" + (j + 1) + "_events.xml\" gui=\"step" + (j + 1) + "_gui.xml\" customs=\"step" + (j + 1) + "_customs.xml\"></Step>";
+
+                            }
+                            checkboxItems item = new checkboxItems();
+                            item.filepath = txtOutputDir.Text + @"\step" + (j + 1) + "_events.xml";
+                            item.stepName = "step" + (j + 1) + "_events.xml";
+                            item.stepTitle = collection[j].Groups[2].ToString();
+                            if (j == 0) {
+                                item.startStepId = 0;
+                            } else {
+                                item.startStepId = items[j-1].endStepId+1;
+                            }
+                            item.endStepId = Convert.ToInt32(collection[j].Groups[1].Value);
+                            item.stepEvents = splitArray[j];
+                            items.Add(item);
+                        } finally {
+                            if (writer != null) {
+                                writer.Close();
+                            }
+                            if (guiWriter != null) {
+                                guiWriter.Close();
+                            }
+                            if (customsWriter != null) {
+                                customsWriter.Close();
+                            }
+                        }
+                    } else {
+                        StreamWriter writer = new StreamWriter(txtOutputDir.Text + @"\step" + (j + 1) + "_events.xml");
+                        try {
+
+                            writer.Write(splitArray[j]);
+
+                            if (string.IsNullOrEmpty(sessionKey)) {
+
+                                eventsFile = eventsFile + "\r\n <Step id=\"" + (j + 1) + "\" label=\"Exit\" events=\"step" + (j + 1) + "_events.xml\"></Step>";
+
+                            } else {
+                                eventsFile = eventsFile + "\r\n <Step id=\"" + (j + 1) + "\" sessionKey=\"" + sessionKey + "\" label=\"Exit\" events=\"step" + (j + 1) + "_events.xml\"></Step>";
+
+
+                            }
+
+                            //remove customs and gui for exit step
+                            File.Delete(txtOutputDir.Text + @"\step" + (j + 1) + "_gui.xml");
+                            File.Delete(txtOutputDir.Text + @"\step" + (j + 1) + "_gui.xml");
+                            checkboxItems item = new checkboxItems();
+                            item.filepath = txtOutputDir.Text + @"\step" + (j + 1) + "_events.xml";
+                            item.stepName = "step" + (j + 1) + "_events.xml";
+                            item.startStepId = items[j - 1].endStepId + 1;
+                            item.endStepId= 999;
+                            item.stepTitle = "Exit";
+                            item.stepEvents = splitArray[j];
+                            items.Add(item);
+                            // chkLstAllStepEvents.Items.Add(item);
+                        } finally {
+                            if (writer != null) {
+                                writer.Close();
+                            }
+                        }
+
+                    }
+
+                }
+            } finally {
+                if (reader != null) {
+                    reader.Close();
+                }
+            }
+
+
+
+            StreamWriter eventsfilesWriter = new StreamWriter(txtOutputDir.Text + @"\eventsfiles.xml");
+            try {
+                eventsFile = eventsFile + "</Steps>";
+                eventsfilesWriter.Write(eventsFile);
+                oldEventsFile = eventsFile;
+            } finally {
+                if (eventsfilesWriter != null) {
+                    eventsfilesWriter.Close();
+                }
+            }
+        }
+
+        string oldEventsFile = string.Empty;
+
+        private void btnStart_Click(object sender, EventArgs e) {
+            string[] directories = Directory.GetDirectories(txtInputDir.Text);
+            for (int i = 0; i < directories.Length; i++) {
+                string[] textFile = Directory.GetFiles(directories[i], "*.txt");
+                string[] xmlFile = Directory.GetFiles(directories[i], "*.xml");
+                List<checkboxItems> items = new List<checkboxItems>();
+                StartSplittingFile(xmlFile[0], "1313", out items);
+
+                string configRead = FrontendUtils.ReadFile(textFile[0]);
+                string[] parts = configRead.Split(new string[] { "\n"},StringSplitOptions.RemoveEmptyEntries);
+                string buildText = string.Empty;
+                string testCaseFolderName = Path.GetFileNameWithoutExtension(directories[i]);
+                for (int j = 0; j < parts.Length; j++) {
+                    //part like {parenttitle}{1:3}
+                    Regex regex = new Regex("\\{(.*?)\\}\\{(\\d+):(\\d+)\\}");
+                    List<checkboxItems> selectedItems = GetSelectedItems(regex.Match(parts[j]).Groups[2].Value, regex.Match(parts[j]).Groups[3].Value,items);
+                    
+                    
+                    GenerateSplitFile(selectedItems, "sessionKey" + j, (j + 1) + "-" + regex.Match(parts[j]).Groups[1].Value.Replace(" ", "_"), 1, testCaseFolderName);
+                    for (int l = 0; l < selectedItems.Count; l++) {
+                        CleanOutputDir(selectedItems[l]);
+                    }
+                    buildText = buildText +
+                        "<automation.macroplayback eventsfile=\""+
+                        "${datastore}.macros."+(j+1)+"-" + regex.Match(parts[j]).Groups[1].Value.Replace(" ", "_")+".eventsfiles.xml\""+
+                        " kernelsteptitle=\"" + regex.Match(parts[j]).Groups[1].Value + "\" createstep=\"false\" />\r\n";
+                }
+                FrontendUtils.WriteFile(directories[i] + @"\buildFile.txt", buildText);
+         
+                //List<checkboxItems> selectedItems = new List<checkboxItems>();
+                //GenerateSplitFile(selectedItems, "sessionKey", "outFolderName", 1);
+            }
+
+        }
+
+        private List<checkboxItems> GetSelectedItems(string from, string to, List<checkboxItems> items) {
+            int fromEvent = Convert.ToInt32(from);
+            int toEvent = Convert.ToInt32(to);
+            List<checkboxItems> returnedItems = new List<checkboxItems>();
+
+            for (int i = 0; i < items.Count; i++) {
+                if ((items[i].startStepId >= fromEvent) && (items[i].endStepId <= toEvent)) {
+                    returnedItems.Add(items[i]);
+                }
+
+
+            }
+
+            return returnedItems;
+
+
+        }
+        private void CleanOutputDir(checkboxItems checkboxItems) {
+            File.Delete(checkboxItems.filepath);
+            string parentDirectory = Directory.GetParent(checkboxItems.filepath).ToString();
+            if (File.Exists(parentDirectory + @"\" + Path.GetFileName(checkboxItems.filepath).Replace("events", "customs"))) {
+                File.Delete(parentDirectory + @"\" + Path.GetFileName(checkboxItems.filepath).Replace("events", "customs"));
+            }
+            if (File.Exists(parentDirectory + @"\" + Path.GetFileName(checkboxItems.filepath).Replace("events", "gui"))) {
+                File.Delete(parentDirectory + @"\" + Path.GetFileName(checkboxItems.filepath).Replace("events", "gui"));
+            }
+        }
+
+        private void GenerateSplitFile(List<checkboxItems> selectedItems, string sessionKey, string folderName, int startIndex, string testCaseFolderName) {
+            string eventsFileDirectory = Directory.GetParent(selectedItems[0].filepath)+@"\"+testCaseFolderName + @"\" + folderName;
+            Directory.CreateDirectory(eventsFileDirectory);
+
+            string eventsFileText = "<Steps label=\"Automatically Generated\">";
+
+
+            for (int i = startIndex - 1, j = 0; i < (selectedItems.Count + startIndex - 1); i++, j++) {
+                WriteCustomsFile(eventsFileDirectory, i + 1);
+                WriteGuiFile(eventsFileDirectory, i + 1);
+                WriteStepEventFile(eventsFileDirectory, i + 1, selectedItems[j]);
+                if (string.Equals(selectedItems[j].stepTitle, "Exit")) {
+                    if (string.IsNullOrEmpty(sessionKey)) {
+                        eventsFileText = eventsFileText + "\r\n <Step id=\"" + (i + 1) + "\" label=\"Exit\" events=\"step" + (i + 1) + "_events.xml\" ></Step>";
+                    } else {
+                        eventsFileText = eventsFileText + "\r\n <Step id=\"" + (i + 1) + "\" sessionKey=\"" + sessionKey + "\" label=\"Exit\" events=\"step" + (i + 1) + "_events.xml\" ></Step>";
+                    }
+                } else {
+                    if (!string.IsNullOrEmpty(sessionKey)) {
+                        eventsFileText = eventsFileText + "\r\n <Step id=\"" + (i + 1) + "\" sessionKey=\"" + sessionKey + "\" label=\"" + selectedItems[j].stepTitle + "\" events=\"step" + (i + 1) + "_events.xml\" gui=\"step" + (i + 1) + "_gui.xml\" customs=\"step" + (i + 1) + "_customs.xml\"></Step>";
+                    } else {
+                        eventsFileText = eventsFileText + "\r\n <Step id=\"" + (i + 1) + "\" label=\"" + selectedItems[j].stepTitle + "\" events=\"step" + (i + 1) + "_events.xml\" gui=\"step" + (i + 1) + "_gui.xml\" customs=\"step" + (i + 1) + "_customs.xml\"></Step>";
+
+                    }
+                }
+            }
+            WriteEventsFile(sessionKey, eventsFileDirectory, eventsFileText);
+
+        }
+
+        private void WriteStepEventFile(string eventsFileDirectory, int stepNumber, checkboxItems checkboxItems) {
+            StreamWriter writer = new StreamWriter(eventsFileDirectory + @"\step" + stepNumber + "_events.xml");
+            try {
+                writer.Write(checkboxItems.stepEvents);
+            } finally {
+                if (writer != null) {
+                    writer.Flush();
+                    writer.Close();
+                    writer.Dispose();
+                }
+            }
+        }
+
+        private void WriteGuiFile(string eventsFileDirectory, int guiNumber) {
+            StreamWriter guiWriter = new StreamWriter(eventsFileDirectory + @"\step" + guiNumber + "_gui.xml");
+            try {
+                guiWriter.Write("<GuiRoot MXRequest=\"GetFullTree\"></GuiRoot>");
+            } finally {
+                if (guiWriter != null) {
+                    guiWriter.Flush();
+                    guiWriter.Close();
+                    guiWriter.Dispose();
+                }
+            }
+        }
+
+        private void WriteCustomsFile(string eventsFileDirectory, int customsNumber) {
+            StreamWriter customsWriter = new StreamWriter(eventsFileDirectory + @"\step" + customsNumber + "_customs.xml");
+            try {
+                customsWriter.Write(@"<Customs></Customs>");
+            } finally {
+                if (customsWriter != null) {
+                    customsWriter.Flush();
+                    customsWriter.Close();
+                    customsWriter.Dispose();
+                }
+            }
+
+        }
+
+        private void WriteEventsFile(string sessionKey, string eventsFileDirectory, string eventsFileText) {
+            StreamWriter eventsfilesWriter = new StreamWriter(eventsFileDirectory + @"\eventsfiles.xml");
+            try {
+                eventsFileText = eventsFileText + "</Steps>";
+                eventsfilesWriter.Write(eventsFileText);
+            } finally {
+                if (eventsfilesWriter != null) {
+                    eventsfilesWriter.Flush();
+                    eventsfilesWriter.Close();
+                    eventsfilesWriter.Dispose();
+                }
+            }
+
+        }
+
+    }
+}
